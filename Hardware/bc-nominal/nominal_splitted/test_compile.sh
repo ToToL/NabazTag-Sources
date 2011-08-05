8e57937f5108c7c256f2e6a7ec837852c1766bc0
ToToL <thomas.andrejak@gmail.com>
2011-08-06 01:10:32 +0200

    Modification pour compiler et exécuter un nominal.mtl en mode simulation

--- Hardware/bc-nominal/nominal_splitted/test_compile.sh
+++ Hardware/bc-nominal/nominal_splitted/test_compile.sh
@@ -3,10 +3,10 @@
 CURRENTDIR=$(dirname $0)
 cd $CURRENTDIR
 
-COMPILER="$HOME/work/compilateur/trunk/mtl_linux/mtl_comp"
+COMPILER="`pwd`/../../metal/mtl_comp"
 
 ./make_nominal.sh
 
 [ $? -eq 0 ] || { echo "Could not make nominal.mtl" ;  }
 
-"$COMPILER" -s "../nominal.mtl" "/var/www/bytecode"
+"$COMPILER" -s "../nominal.mtl" "../bootc"
